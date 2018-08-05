35436bb32af53a4e375472cdf885acbad8eb6c74
Phoenox <lxl910128@qq.com>
2018-08-05 20:16:35 +0800

    fxI

--- src/main/java/club/projectgaia/varys/domain/po/NewsAbstract.java
+++ src/main/java/club/projectgaia/varys/domain/po/NewsAbstract.java
@@ -15,7 +15,7 @@ public class NewsAbstract {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     //摘要
-    @Column(length = 1024)
+    @Column(columnDefinition = "TEXT")
     private String Abstract;
     //作者
     private String author;
@@ -34,10 +34,15 @@ public class NewsAbstract {
     @Column(length = 1024)
     private String title;
     //关键词
+    @Column(length = 2048)
     private String keyword;
     //图片
     private String pics;
     //分类
     private String newsType;
+    //是否有link
+    private Integer isLink;
+    //nid
+    private String nid;
 
 }
--- src/main/java/club/projectgaia/varys/repository/NewsTypeRepository.java
+++ src/main/java/club/projectgaia/varys/repository/NewsTypeRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import club.projectgaia.varys.domain.po.NewsAbstract;
 import club.projectgaia.varys.domain.po.NewsType;
 
+import java.util.List;
+
 public interface NewsTypeRepository extends JpaRepository<NewsType, Long> {
+    List<NewsType> getAllByNewsType(String newsType);
 }
