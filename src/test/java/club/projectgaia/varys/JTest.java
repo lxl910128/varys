c40f8e75435f56259078a7cf0090b6189007ccae
Phoenix <lxl910128@qq.com>
2019-08-28 18:40:47 +0800

    fix

--- src/test/java/club/projectgaia/varys/JTest.java
+++ src/test/java/club/projectgaia/varys/JTest.java
@@ -1,56 +1,52 @@
 package club.projectgaia.varys;
 
 
-import org.apache.commons.lang3.StringUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import club.projectgaia.varys.domain.po.AVInfo;
-import club.projectgaia.varys.domain.po.AVJob;
-import club.projectgaia.varys.domain.po.AvatarInfo;
 
 
 public class JTest {
 
     @Test
     public void test() throws IOException {
-        File in = new File("H:\\index.tmp");
+        //创建httpClient实例
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        //创建httpGet实例
+        HttpGet httpGet = new HttpGet("https://bj.lianjia.com");
+        //设置代理IP，设置连接超时时间 、 设置 请求读取数据的超时时间 、 设置从connect Manager获取Connection超时时间、
+        HttpHost proxy = new HttpHost("63.249.67.70", 53281);
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setProxy(proxy)
+                //.setConnectTimeout(10000)
+                //.setSocketTimeout(10000)
+                //.setConnectionRequestTimeout(3000)
+                .build();
+        httpGet.setConfig(requestConfig);
+        //设置请求头消息
+        httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36");
+        CloseableHttpResponse response = httpClient.execute(httpGet);
 
-        Document doc = Jsoup.parse(in, "UTF-8");
-
-        Elements waterfalls = doc.select("div#waterfall");
-        Element all = waterfalls.get(1);
-        if (all != null) {
-            Elements newJobElement = all.select("div.item");
-            newJobElement.forEach(x -> {
-                Element y = x.selectFirst("a.movie-box");
-                if (y != null) {
-                    AVJob newJob = new AVJob();
-                    newJob.setUrl(y.attr("href"));
-                    newJob.setTitle(y.selectFirst("div.photo-info").text());
-                }
-            });
-        }
-
-        Element next = doc.selectFirst("ul.pagination");
-        if (next != null) {
-            Element nextP = next.selectFirst("a#next");
-            if (nextP != null) {
-                String nextPage = String.format("https://www.javbus.com%s", nextP.attr("href"));
-                System.out.println(nextPage);
+        if (response != null) {
+            HttpEntity entity = response.getEntity();  //获取返回实体
+            if (entity != null) {
+                System.out.println("网页内容为:" + EntityUtils.toString(entity, "utf-8"));
             }
         }
-
+        if (response != null) {
+            response.close();
+        }
+        if (httpClient != null) {
+            httpClient.close();
+        }
     }
 
 }
